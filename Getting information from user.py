932a5d5a96ba1bd746a1709d4625d4190d64fda6
Adarsh Srivastava <78731304+adarshsri880@users.noreply.github.com>
2024-05-11 13:33:36 +0530

    Getting information from user.py

---
+++ Getting information from user.py
@@ -0,0 +1,25 @@
+# Name = "Adarsh"
+# print ("Hello " + Name)
+
+User_Name = input ("Enter your full name\n")
+Father_Name = input ("Enter your father name\n")
+Age_Name = input ("Enter your Age\n")
+Place_Name = input ("Enter your place name\n")
+Qulaification_Name = input ("Enter your Qulaification\n")
+Hobbies = input ("Enter your Hobbies\n")
+Favourite_Subject = input ("Enter your favourite subject\n")
+Current_job_Profile_or_Looking = input ("Enter your current job profile or looking\n")
+Mobile_Number = input ("Enter your mobile number\n")
+
+print(f'''
+Here is your Details.
+Your full name {User_Name}.
+Your Father's Name is {Father_Name}. 
+You are {Age_Name} years old.
+You are from {Place_Name}.
+You have Completed {Qulaification_Name}.
+Your Hobbies is {Hobbies}.
+Your Favourite subject is {Favourite_Subject}.
+Your current job profile or looking is {Current_job_Profile_or_Looking}.
+Your mobile number is {Mobile_Number}.\n
+Thank You Adarsh For sharing your profile with us.''')
